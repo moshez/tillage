c7af74a0204657b1ef6aa078f528ecfd1da131b4
Moshe Zadka <zadka.moshe@gmail.com>
2015-07-06 20:25:15 -0700

    +license

--- tillage/description.py
+++ tillage/description.py
@@ -6,3 +6,5 @@ import attr
 @attr.s
 class Description(object):
     name = attr.ib(validator=attr.validators.instance_of(str))
+    year = attr.ib(validator=attr.validators.instance_of(int))
+    author = attr.ib(validator=attr.validators.instance_of(str))
--- tillage/tests/test_mkfiles.py
+++ tillage/tests/test_mkfiles.py
@@ -7,7 +7,7 @@ from tillage import contents, description
 class TestContents(unittest.TestCase):
 
     def setUp(self):
-        self.descr = description.Description(name='tillagedemoproject')
+        self.descr = description.Description(name='tillagedemoproject', author='Demo Author', year=1897)
     ##    myTemp = os.environ['TEMP']
     ##    if not os.path.isdir(myTemp):
     ##        os.mkdir(myTemp)
@@ -16,9 +16,19 @@ class TestContents(unittest.TestCase):
     ##    self.fpath = filepath.FilePath(self.dname)
 
     def test_README(self):
+        self.assertIn(contents.getREADME, contents.canonical)
         name, readme = contents.getREADME(self.descr)
         self.assertEquals(name, 'README.rst')
         lines = readme.strip().splitlines()
         self.assertEquals(lines[0], self.descr.name)
         self.assertEquals(set(lines[1]), set('-'))
         self.assertGreaterEqual(len(lines[1]), len(lines[0]))
+
+    def test_LICENSE(self):
+        self.assertIn(contents.getLICENSE, contents.canonical)
+        name, license = contents.getLICENSE(self.descr)
+        self.assertEquals(name, 'LICENSE')
+        lines = [x for x in license.splitlines() if x != '']
+        self.assertEquals(lines[0], 'The MIT License (MIT)')
+        self.assertIn(str(self.descr.year), lines[1])
+        self.assertIn(self.descr.author, lines[1])
